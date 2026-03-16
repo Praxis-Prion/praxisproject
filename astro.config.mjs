fc3412f54b6e25765e6e582dd8d99b0efc7699fd
praxis <chris@praxisproject.dev>
2026-03-15 19:50:23 -0500

    feat: add pagefind search with dropdown results

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
+
 export default defineConfig({
   vite: {
     server: {
       allowedHosts: ['dev.praxisproject.dev']
     }
   },
+  integrations: [pagefind()],
 });
