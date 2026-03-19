2b53580e9fa05776532357220844601e872c19f9
praxis <chris@praxisproject.dev>
2026-03-19 16:16:34 -0500

    feat: support for post themes
    -content.config.ts: added optional theme to schema
    BaseLayout: receives theme class for <body>
    MarkdownPostLayout: sends theme to BaseLayout
    global.css: contains the css for themes

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,18 +6,19 @@ import { defineCollection } from "astro:content";
 import { z } from "astro/zod";
 
 const blog = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content" }),
-    schema: ({ image }) => z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-      image: z.object({
-        url: image(),
-        alt: z.string()
-      }).optional(),
-      tags: z.array(z.string())
-    })
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content" }),
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: image(),
+      alt: z.string()
+    }).optional(),
+    tags: z.array(z.string()),
+    theme: z.string().optional()
+  })
 });
 
 export const collections = { blog };
